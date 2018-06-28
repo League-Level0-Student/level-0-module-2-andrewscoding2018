251e91bbb7cd43920f015a969f1fa60844fdbc89
andrewscoding2018 <league@WTS7.attlocal.net>
2018-06-28 11:59:14 -0700

    Mod 2

---
+++ src/methods/Rollercoaster.java
@@ -0,0 +1,13 @@
+package methods;
+
+import javax.swing.JOptionPane;
+
+public class Rollercoaster {
+public static void main(String[] args) {
+	String height = JOptionPane.showInputDialog("What is your height in inches?");
+			if(height>=48)
+			{
+				
+			}
+}
+}
